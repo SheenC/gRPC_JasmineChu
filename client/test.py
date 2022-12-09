61e213bf9274ddd58a16a0ce5df427bac091a226
SheenC <jascxy@hotmail.com>
2022-12-09 00:24:18 -0500

    Added test cases with server on

--- client/test.py
+++ client/test.py
@@ -11,16 +11,16 @@ class MockClient(object):
     def __init__(self):
         pass
         self.mockClient = InventoryClient()
-        self.mockClient.getBookName = MagicMock(return_value = 'The other book')
+        self.mockClient.getBookName = MagicMock(request="", return_value = 'The other book')
 
-# Test inventory_client
+# Test inventory_client without starting server
 class TestInventoryClient(unittest.TestCase):
     @patch('inventory_client.InventoryClient.getBookName')
     def testInventoryClient(self, mock_getBookName):
         mockClient = MockClient().mockClient
         self.assertEqual(mockClient.getBookName(ISBN='ISBN6789', key='value'), 'The other book')
 
-# Test get_book_titles
+# Test get_book_titles without starting server
 class TestGetBookTitles(unittest.TestCase):
     @patch('get_book_titles.GetBookTitles.getBookNames')
     def testGetBookTitles(self, mock_getBookTitles):
@@ -28,6 +28,18 @@ class TestGetBookTitles(unittest.TestCase):
         getBookTitles.getBookNames = MagicMock(return_value = ['The other book','The anotther book'])
         self.assertEqual(getBookTitles.getBookNames(ISBNs=['ISBN6789','ISBN5678'], key='value'), ['The other book', 'The anotther book'])
 
+# Test inventory_client starting server
+class TestInventoryClientWithServerOn(unittest.TestCase):
+    def testInventoryClient(self):
+        client = InventoryClient()
+        self.assertEqual(client.getBookName(ISBN='ISBN0011'), 'Book One')
+        self.assertEqual(client.getBookName(ISBN='ISBN6789'), '[Not Found]')
+
+# Test get_book_titles starting server
+class TestGetBookTitlesWithServerOn(unittest.TestCase):
+    def testGetBookTitles(self):
+        client = GetBookTitles(mockClient=None)
+        self.assertEqual(client.getBookNames(ISBNs=['ISBN0011', 'ISBN6789']), ['Book One', '[Not Found]'])
 
 if __name__ == '__main__':
     unittest.main()
