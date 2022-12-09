1c563ab0b8410870cf76d8a6b79a038587b9cb15
SheenC <jascxy@hotmail.com>
2022-12-08 21:59:02 -0500

    Added GetBookNames in Client

--- client/get_book_titles.py
+++ client/get_book_titles.py
@@ -1,6 +1,26 @@
 import os, sys
 sys.path.insert(0, os.path.abspath("."))
-import library_pb2
-import library_pb2_grpc
-import service.InventoryService
+from inventory_client import InventoryClient
+
+class GetBookTitles(object):
+    """
+    Given a list of ISBN, return a list of book names
+    """
+
+    def __init__(self):
+        self.client = InventoryClient()
+
+    def getBookNames(self, ISBNs):
+        names = []
+        for isbn in ISBNs:
+            name = self.client.getBookName(ISBN=isbn)
+            names.append(name)
+            
+        return names
+
+
+if __name__ == '__main__':
+    getBookTitles = GetBookTitles()
+    result = getBookTitles.getBookNames(ISBNs=["ISBN0001","ISBN0011"])
+    print("The name is: ", result)
 
--- client/inventory_client.py
+++ client/inventory_client.py
@@ -3,7 +3,6 @@ import os, sys
 sys.path.insert(0, os.path.abspath("."))
 import library_pb2
 import library_pb2_grpc
-import service.InventoryService
 import re
 
 
