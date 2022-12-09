c9217b477629d445bbb79d503296a6f024902a0b
SheenC <jascxy@hotmail.com>
2022-12-09 00:46:44 -0500

    Added Side Effects of Mock

--- client/test.py
+++ client/test.py
@@ -6,40 +6,76 @@ from get_book_titles import GetBookTitles
 from unittest.mock import MagicMock, patch
 
 # Create a mock of the API class interface defined at inventory_client
-class MockClient(object):
+class MockInventoryClient(object):
 
     def __init__(self):
-        pass
-        self.mockClient = InventoryClient()
-        self.mockClient.getBookName = MagicMock(request="", return_value = 'The other book')
 
-# Test inventory_client without starting server
+        def side_effect_func(ISBN):
+            ISBN_title_dict = {'ISBN0011': 'Book One', 'ISBN0012': 'Book Two', 'ISBN0013': 'Book Three'}
+            if ISBN in ISBN_title_dict.keys():
+                return ISBN_title_dict[ISBN]
+            else:
+                return '[Not Found]'
+
+        self.mockClient = InventoryClient()
+        self.mockClient.getBookName = MagicMock(side_effect=side_effect_func)
+
+
+# Create a mock API of get_book_titles
+class MockGetBookTitles(object):
+
+    def __init__(self):
+
+        def side_effect_func(ISBNs):
+            ISBN_title_dict = {'ISBN0011': 'Book One', 'ISBN0012': 'Book Two', 'ISBN0013': 'Book Three'}
+            titles = []
+            for ISBN in ISBNs:
+                
+                if ISBN in ISBN_title_dict.keys():
+                    titles.append(ISBN_title_dict[ISBN])
+                else:
+                    titles.append('[Not Found]')
+            return titles
+
+        self.mockClient = GetBookTitles()
+        self.mockClient.getBookNames = MagicMock(side_effect=side_effect_func)
+ 
+
+# Test inventory_client with a mock client
 class TestInventoryClient(unittest.TestCase):
     @patch('inventory_client.InventoryClient.getBookName')
     def testInventoryClient(self, mock_getBookName):
-        mockClient = MockClient().mockClient
-        self.assertEqual(mockClient.getBookName(ISBN='ISBN6789', key='value'), 'The other book')
+        mockClient = MockInventoryClient().mockClient
+        self.assertEqual(mockClient.getBookName('ISBN0011'), 'Book One')
+        self.assertEqual(mockClient.getBookName('ISBN0012'), 'Book Two')
+        self.assertEqual(mockClient.getBookName('ISBN0013'), 'Book Three')
+        self.assertEqual(mockClient.getBookName('ISBN6789'), '[Not Found]')
 
-# Test get_book_titles without starting server
+
+# Test get_book_titles with a mock client
 class TestGetBookTitles(unittest.TestCase):
     @patch('get_book_titles.GetBookTitles.getBookNames')
     def testGetBookTitles(self, mock_getBookTitles):
-        getBookTitles = GetBookTitles()
-        getBookTitles.getBookNames = MagicMock(return_value = ['The other book','The anotther book'])
-        self.assertEqual(getBookTitles.getBookNames(ISBNs=['ISBN6789','ISBN5678'], key='value'), ['The other book', 'The anotther book'])
+        getBookTitles = MockGetBookTitles().mockClient
+        self.assertEqual(getBookTitles.getBookNames(['ISBN0011','ISBN0012', 'ISBN6789']), ['Book One', 'Book Two', '[Not Found]'])
 
-# Test inventory_client starting server
+
+# Test inventory_client with a live server
 class TestInventoryClientWithServerOn(unittest.TestCase):
     def testInventoryClient(self):
         client = InventoryClient()
         self.assertEqual(client.getBookName(ISBN='ISBN0011'), 'Book One')
+        self.assertEqual(client.getBookName(ISBN='ISBN0012'), 'Book Two')
+        self.assertEqual(client.getBookName(ISBN='ISBN0013'), 'Book Three')
         self.assertEqual(client.getBookName(ISBN='ISBN6789'), '[Not Found]')
 
-# Test get_book_titles starting server
+
+# Test get_book_titles with a live server
 class TestGetBookTitlesWithServerOn(unittest.TestCase):
     def testGetBookTitles(self):
         client = GetBookTitles()
         self.assertEqual(client.getBookNames(ISBNs=['ISBN0011', 'ISBN6789']), ['Book One', '[Not Found]'])
+
 
 if __name__ == '__main__':
     unittest.main()
